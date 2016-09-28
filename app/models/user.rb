b14e062325bd0da2ef2966772134a7e2cf7aef89
Pablo Vallejo <pabloninety@gmail.com>
2016-09-28 14:46:28 -0500

    :sunny: Add additional fields to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates_inclusion_of :kind, :in => %w(teacher student),
+    :message => "%{value} is not a valid type"
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
