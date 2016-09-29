d22fd7dd5f832458250911de6a49459bd1c49417
Pablo Vallejo <pabloninety@gmail.com>
2016-09-29 14:26:38 -0500

    :white_check_mark: Add basic home test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
   test "should get index" do
-    get home_index_url
+    sign_in @user
+    get root_path
     assert_response :success
   end
 
